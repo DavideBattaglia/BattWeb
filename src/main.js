f2f2e6afb848a8713c8b6bc4c125ef662de76ebf
Davide Battaglia <davide.battaglia-1@studenti.unitn.it>
2024-02-23 16:50:38 +0100

    version 0.0

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/DQjjkyinO5qoEV-t/scene.splinecode');
