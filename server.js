557c8e615c88fc8b6e41907912550ef777fdacf5
Davide Battaglia <davide.battaglia-1@studenti.unitn.it>
2024-03-04 17:49:56 +0100

    MobileCommit

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const userAgent = require('express-useragent');
 const port = 3000;
 
 app.use(express.static(__dirname));
 
+// Middleware per l'analisi dell'user agent
+app.use(userAgent.express());
+
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  // Ottieni l'user agent dal request
+  const ua = req.useragent;
+
+  // Controlla se si tratta di un dispositivo mobile
+  if (ua.isMobile) {
+    // Restituisci il file index per dispositivi mobili
+    res.sendFile(path.join(__dirname, './dist/index.html'));
+  } else {
+    // Restituisci il file index per PC
+    res.sendFile(path.join(__dirname, 'index.html'));
+  }
 });
 
 app.listen(port, () => {
